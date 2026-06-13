07f95ad2e545b6e03f8ca2a2f9a5544a77e21451
chika <chika.ejiofor@bazaratech.com>
2026-06-13 11:39:19 +0100

    url change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://znimi-backend-production.up.railway.app/api'
+  // apiUrl: 'http://localhost:5000/api'
 };
